cc958c6699fb7f93deb3a179479d6d16fb4457ea
Howard Kwon <howie.ykwon70@gmail.com>
2021-07-20 19:38:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+
+
+
+function superbowlWin(array) {
+    let winGame = array.find(function(game) {return game.result === "W"});
+    if (winGame) {
+      return winGame.year
+    } else {
+      return undefined
+    }
+  }
